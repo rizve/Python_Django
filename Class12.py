80248b39e6fa50eec4fbecfa42fde0ec23b6d51b
rizve <rizve.cse@gmail.com>
2023-07-18 00:13:12 +0600

    Python Basic Review Class

---
+++ Class12.py
@@ -0,0 +1,132 @@
+# print("Hello World") # This is my First Program.
+
+# x = 10
+# X = 11
+
+# print(x)
+# print(X)
+# String    ------- Text type
+# Intiger, Float, Complex   ------- Numeric
+# lsit, Tuplem range ----- Sequence Type
+# Dictionary ------ Mapping Type
+# set --------- set Type
+# Boolean Type
+# Binary Type
+# None Type
+
+
+# x = ''' hjkahsjkdfh
+#             jhjkhaksdjf 
+#                 jkhakjsdfh'''
+
+
+# x = 'Bangladesh is our Mother land'
+# x = 'bangladesh is our mother land'
+
+# print(type(x))
+# print(x.upper())
+# print(x.capitalize())
+# print(x.title())
+# print(x[0:6])
+# print(x[14:18])
+# print(x.count("a"))
+# print(x.index("u"))
+
+# Ture and False
+# 0,1
+
+# x = [12,"Bangladesh", 10.23]
+# x = [10,2,123,234,666,34,21]
+
+# print(x[2])
+# x.append("india")
+# x.append(232)
+# x.sort()
+# x.remove(666)
+# x.insert(3,"London")
+# x.pop(4)
+# print(x)
+
+
+# x.append("apple")
+
+# x = (2323, "Abc",10.23)
+
+# x = {
+#     "bangla":70,
+#     "English":80,
+#     "Math": 90
+# }
+
+# x["English"] = 75
+
+# print(x)
+
+# bangla = 70
+# English = 80
+# Math = 90
+
+# if and else
+
+# x = 10
+
+# y = 10
+
+# if x == y:
+#     print("This is a real Number.")
+# else:
+#     print("Try Again")
+
+# x = input()
+# print(x.lower())
+
+# x = float(input())
+# y = float(input())
+
+# print(int(x) + int(y))
+# print(x + y)
+
+# if x < y:
+#     print("Y is a bigger number")
+# elif x > y:
+#     print("X is a bigger Number")
+# else:
+#     print("Try Again")
+
+# while True:
+#     print("test")
+
+# i = 0
+# while i < 6:
+    # i = i+1
+    # if i == 3:
+    #     break
+    # i += 1
+    # if i == 3:
+    #     continue
+    # print(i)
+
+x = [12,234,234,234,234,234,"Bangladesh", 10.23]
+
+# x = "Bangladesh"
+
+
+# for i in x:
+#     if i == "Bangladesh":
+#         # break
+#         continue
+#     print(i)
+
+# print(len(x))
+        
+for i in range(1,11):
+    print(i)
+
+
+
+
+
+
+
+
+
